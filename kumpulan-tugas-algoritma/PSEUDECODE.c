13c0aebbc36e92954defc9bb45d6f9e6bf925438
sitisolihah15 <sitisolihahl177@gmail.com>
2026-06-11 15:11:51 +0700

    Create PSEUDECODE.c

---
+++ kumpulan-tugas-algoritma/PSEUDECODE.c
@@ -0,0 +1,14 @@
+PSEUDOCODE
+Algoritma Menentukan_kelulusan
+( menentukan nilai kelulusan anda termasuk lulus atau  gagal )
+
+Kamus
+n : intenger
+
+Deskripsi
+Read (nilai)
+If nilai > 50 then 
+	Write (“ anda lulus”)
+Else 
+	Write (“anda gagal”)
+Endif
